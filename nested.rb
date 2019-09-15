568a5f1f1b0c74095bb59404aeefc744289d6a58
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-15 12:00:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -71,11 +71,7 @@ def adding_matz
 # }
 
 	programmer_hash = 
- 		{ 
- 		  :yukihiro_matsumoto => {
-      :known_for => "Ruby", 
-      :languages => ["LISP", "C"]
- 		  },
+ 		{
  		  
         :grace_hopper => {
           :known_for => "COBOL",
@@ -91,6 +87,10 @@ def adding_matz
         }
      }
      
+     programmer_hash[:yukihiro_matsumoto] = {}
+     programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+     programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+     programmer_hash
     
 
     
